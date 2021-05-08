33500a34761cea04ebe8ee0f6e003bc1f4674f45
the2sang <the2sang@gmail.com>
2021-05-08 09:05:16 +0900

    add process id

--- src/main/java/com/kepco/ppa/web/PPABatchApp.java
+++ src/main/java/com/kepco/ppa/web/PPABatchApp.java
@@ -14,6 +14,7 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.env.Environment;
 import tech.jhipster.config.DefaultProfileUtil;
@@ -67,6 +68,7 @@ public class PPABatchApp {
      */
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(PPABatchApp.class);
+        app.addListeners(new ApplicationPidFileWriter());
         DefaultProfileUtil.addDefaultProfile(app);
         Environment env = app.run(args).getEnvironment();
         logApplicationStartup(env);
