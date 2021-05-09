c449424a928e701b5f26fbefb1a53b8e6f6e6795
the2sang <the2sang@gmail.com>
2021-05-09 16:46:34 +0900

    modify JobConfiguration add fail status end

--- src/main/java/com/kepco/ppa/web/batch/listener/LoggingStepStartStopListener.java
+++ src/main/java/com/kepco/ppa/web/batch/listener/LoggingStepStartStopListener.java
@@ -1,5 +1,9 @@
 package com.kepco.ppa.web.batch.listener;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.annotation.AfterStep;
@@ -8,16 +12,33 @@ import org.springframework.batch.core.annotation.BeforeStep;
 /**
  * @author Michael Minella
  */
+@Slf4j
 public class LoggingStepStartStopListener {
 
     @BeforeStep
     public void beforeStep(StepExecution stepExecution) {
-        System.out.println(stepExecution.getStepName() + " has begun!");
+        log.info(stepExecution.getStepName() + " has begun!");
     }
 
     @AfterStep
     public ExitStatus afterStep(StepExecution stepExecution) {
-        System.out.println(stepExecution.getStepName() + " has ended!");
+        SimpleDateFormat dtFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        StringBuffer message = new StringBuffer();
+        String startFormatDate = "";
+        String endFormatDate = "";
+
+        if (stepExecution.getStartTime() != null && stepExecution.getEndTime() != null) {
+            startFormatDate = dtFormat.format(stepExecution.getStartTime());
+            endFormatDate = dtFormat.format(stepExecution.getEndTime());
+            message.append(stepExecution.getStepName() + " - StartTime:" + startFormatDate + " / EndTime:" + endFormatDate);
+        }
+
+        message.append("  Step Status:" + stepExecution.getStatus());
+        message.append("  readData:" + stepExecution.getReadCount());
+
+        log.info("Step Information - " + message.toString());
+
+        log.info(stepExecution.getStepName() + " has ended!");
 
         return stepExecution.getExitStatus();
     }
