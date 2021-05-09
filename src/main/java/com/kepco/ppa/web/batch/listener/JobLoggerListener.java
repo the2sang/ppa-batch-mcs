d53554c079f6ec7fccc607b0d529ce3418c2de41
the2sang <the2sang@gmail.com>
2021-05-09 16:46:18 +0900

    modify JobConfiguration add fail status end

--- src/main/java/com/kepco/ppa/web/batch/listener/JobLoggerListener.java
+++ src/main/java/com/kepco/ppa/web/batch/listener/JobLoggerListener.java
@@ -1,5 +1,7 @@
 package com.kepco.ppa.web.batch.listener;
 
+import java.text.SimpleDateFormat;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.annotation.AfterJob;
 import org.springframework.batch.core.annotation.BeforeJob;
@@ -26,7 +28,7 @@ import org.springframework.batch.core.annotation.BeforeJob;
 //				jobExecution.getStatus()));
 //	}
 //}
-
+@Slf4j
 public class JobLoggerListener {
 
     private static String START_MESSAGE = "%s is beginning execution";
@@ -34,11 +36,22 @@ public class JobLoggerListener {
 
     @BeforeJob
     public void beforeJob(JobExecution jobExecution) {
-        System.out.println(String.format(START_MESSAGE, jobExecution.getJobInstance().getJobName()));
+        log.info(String.format(START_MESSAGE, jobExecution.getJobInstance().getJobName()));
     }
 
     @AfterJob
     public void afterJob(JobExecution jobExecution) {
-        System.out.println(String.format(END_MESSAGE, jobExecution.getJobInstance().getJobName(), jobExecution.getStatus()));
+        SimpleDateFormat dtFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        StringBuffer message = new StringBuffer();
+
+        String startFormatDate = dtFormat.format(jobExecution.getStartTime());
+        String endFormatDate = dtFormat.format(jobExecution.getEndTime());
+
+        message.append(jobExecution.getJobInstance() + " - StartTime:" + startFormatDate + " / EndTime:" + endFormatDate);
+        message.append("  Job Status:" + jobExecution.getStatus());
+
+        log.info("  Job Information - " + message.toString());
+
+        log.info(String.format(END_MESSAGE, jobExecution.getJobInstance().getJobName(), jobExecution.getStatus()));
     }
 }
