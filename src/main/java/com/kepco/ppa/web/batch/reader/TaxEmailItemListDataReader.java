9c1ee7d37b2ab9afef55cd6d1fa48c244d91d5c0
KHC <the2sang@gmail.com>
2021-06-17 07:29:11 +0900

    조회조건 날짜범위 31 추가

--- src/main/java/com/kepco/ppa/web/batch/reader/TaxEmailItemListDataReader.java
+++ src/main/java/com/kepco/ppa/web/batch/reader/TaxEmailItemListDataReader.java
@@ -50,7 +50,7 @@ public class TaxEmailItemListDataReader {
         sb.append("B.MAIL_GUB_CODE = '2'"); // 개별메일인 경우 (1-대표메일)
         sb.append("   AND B.MAIL_STATUS_CODE = '99' "); //메일진행상태(null:DEFAULT,'01':작성중및회계처리중,'02'회계처리완료,'98':공급받는자반려
         sb.append("   AND (B.INVOICEE_CONTACT_EMAIL1 LIKE '%ppa%' OR B.INVOICEE_CONTACT_EMAIL2 LIKE '%ppa%') "); // 공급자 이메일이 ppa로 시작
-        //sb.append("  AND B.ISSUE_DT >= TO_CHAR(sysdate - 31, 'YYYYMMDDhh24miss')"); // 30일전 데이터까지 조회
+        sb.append("  AND B.ISSUE_DT >= TO_CHAR(sysdate - 31, 'YYYYMMDDhh24miss')"); // 30일전 데이터까지 조회
         return sb.toString();
     }
 }
